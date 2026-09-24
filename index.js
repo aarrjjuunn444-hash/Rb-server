8c728fc6ffe22a04a47b59b2131aa9ce7174d090
unknown <ARJUN>
2026-09-24 09:43:42 +0530

    Rb-server

---
+++ index.js
@@ -0,0 +1,13 @@
+const jsonServer= require('json-server')
+const rbServer = jsonServer.create()
+const router = jsonServer.router('db.jason')
+const middleware = jsonServer.defaults()
+
+const PORT = 3000
+
+rbServer.use(middleware)
+rbServer.use(router)
+
+rbServer.listen(PORT,()=>{
+    console.log(`Rb server started running at PORT:${PORT} successfully...`)
+})
